b0c5381ddbbb1823ca5519fab11d224bd188cbc9
ArinaKotyaeva <m2405866@edu.misis.ru>
2024-12-02 22:59:28 +0300

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./index.css";
+import SignUp from "./components/SignUp";
+
+
+function App() {
+  return (
+    <div className="App">
+        <SignUp></SignUp>
+    </div>
+  );
+}
+
+export default App;
