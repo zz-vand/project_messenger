efb991eb9b3af86fa93afd701084426a0cb44837
kirachekaeva <m2408945@edu.misis.ru>
2024-12-08 00:46:21 +0900

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,22 @@
 import React from "react";
 import "./index.css";
 import SignUp from "./components/SignUp";
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Register from "./components/Register";
 
 function App() {
   return (
+    <>
     <div className="App">
-        <SignUp></SignUp>
+        <SignUp/>
     </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="registr" element={<Register/>}/>
+      <Route path="signup" element={<SignUp/>}/>
+    </Routes>
+    </BrowserRouter>
+    </>
   );
 }
 
